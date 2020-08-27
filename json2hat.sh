fb183a7a1fe1d6f799dc6c8850478385b84d9094
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-08-27 16:05:20 +0000

    Hopefully the final state
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- json2hat.sh
+++ json2hat.sh
@@ -17,12 +17,12 @@ if [ -z "${SYNC_URL}" ]
 then
   export SYNC_URL="`cat ./secrets/SYNC_URL.${env}.secret`"
 fi
-export REPO_ACCESS="`cat ./secrets/REPO_ACCESS.secret`"
-# export NO_PROFILE_UPDATE=1
-export REPLACE=1
-# export ONLY_GITHUB=1
 # export DRY_RUN=1
 # export SKIP_BOTS=1
+export REPO_ACCESS="`cat ./secrets/REPO_ACCESS.secret`"
+export NO_PROFILE_UPDATE=1
+export REPLACE=1
+export ONLY_GITHUB=1
 # export NAME_MATCH=0|1|2
-export NAME_MATCH=2
+export NAME_MATCH=1
 ./json2hat
